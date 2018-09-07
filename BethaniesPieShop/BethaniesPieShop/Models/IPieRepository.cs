06b3e154f6695d27a37788b71a0def2c1942949c
Luc Leysen <luc56.leysen@gmail.com>
2018-09-07 16:26:34 +0200

    Add Model

---
+++ BethaniesPieShop/BethaniesPieShop/Models/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace BethaniesPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
--- BethaniesPieShop/BethaniesPieShop/Startup.cs
+++ BethaniesPieShop/BethaniesPieShop/Startup.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BethaniesPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BethaniesPieShop
@@ -15,6 +11,7 @@ namespace BethaniesPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockedPieRepository>();
             services.AddMvc();
         }
 
