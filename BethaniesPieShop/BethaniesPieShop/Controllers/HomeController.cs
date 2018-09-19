c738fbc83c8827357321e49c9a7833ccdf4afc1b
Luc Leysen <luc56.leysen@gmail.com>
2018-09-19 17:12:03 +0200

    Add HomeView

---
+++ BethaniesPieShop/BethaniesPieShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using BethaniesPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethaniesPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie overview";
+
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            return View(pies);
+        }
+    }
+}
--- BethaniesPieShop/BethaniesPieShop/Models/IPieRepository.cs
+++ BethaniesPieShop/BethaniesPieShop/Models/IPieRepository.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace BethaniesPieShop.Models
 {
