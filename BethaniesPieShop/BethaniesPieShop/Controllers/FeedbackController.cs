705be18dd31b8c570845257e6b93b44c11bebed2
Luc Leysen <luc56.leysen@gmail.com>
2018-09-24 13:52:24 +0200

    add feedback

--- BethaniesPieShop/BethaniesPieShop/Controllers/FeedbackController.cs
+++ BethaniesPieShop/BethaniesPieShop/Controllers/FeedbackController.cs
@@ -16,5 +16,23 @@ namespace BethaniesPieShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _feedbackRepository.AddFeedback(feedback);
+
+                return RedirectToAction("FeedbackComplete");
+            }
+
+            return View(feedback);
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
     }
 }
