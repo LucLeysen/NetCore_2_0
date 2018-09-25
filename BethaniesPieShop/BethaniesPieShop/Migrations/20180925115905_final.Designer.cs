6a003d53173b19730f01f371b9cc4cce5f294a3e
Luc Leysen <luc56.leysen@gmail.com>
2018-09-25 14:46:15 +0200

    Authentication

--- BethaniesPieShop/BethaniesPieShop/Migrations/20180925115905_final.Designer.cs
+++ BethaniesPieShop/BethaniesPieShop/Migrations/20180925115905_final.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BethaniesPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180925115905_final")]
+    partial class final
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
